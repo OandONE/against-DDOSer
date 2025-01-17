42a2ca610aad759bec07d2ddb90df3c866dd7289
01 <159622426+OandONE@users.noreply.github.com>
2025-01-17 09:47:48 +0330

    Add files via upload

--- not-DDOSer.py
+++ not-DDOSer.py
@@ -1,37 +1,38 @@
-from bottle import request, response, run, route, hook
+from bottle import request, abort, run, route, hook
 import time
 
 rate_limits = {}
-BLOCK_TIME = 60  # زمان بلاک در ثانیه
-MAX_REQUESTS = 7  # حداکثر درخواست‌ها در 30 ثانیه
-TIME_FRAME = 30  # بازه زمانی در ثانیه
+BLOCK_TIME = 60  
+MAX_REQUESTS = 7  
+TIME_FRAME = 30  
 
-#for ever request
 @hook('before_request')
 def check_rate_limit():
     client_ip = request.remote_addr
     current_time = time.time()
+    
     if client_ip not in rate_limits:
         rate_limits[client_ip] = {
             'count': 0,
             'first_request_time': current_time,
             'blocked_until': 0
         }
+        
     user_info = rate_limits[client_ip]
+    
     if user_info['blocked_until'] > current_time:
-        response.status = 403
-        response.body = "شما به مدت 60 ثانیه مسدود شده‌اید."
-        return
+        abort(403, "شما به مدت 60 ثانیه مسدود شده‌اید.")
+        
     if current_time - user_info['first_request_time'] < TIME_FRAME:
         user_info['count'] += 1
     else:
         user_info['count'] = 1
         user_info['first_request_time'] = current_time
+        
     if user_info['count'] > MAX_REQUESTS:
         user_info['blocked_until'] = current_time + BLOCK_TIME
-        response.status = 403
-        response.body = "شما به مدت 60 ثانیه مسدود شده‌اید."
-        return
+        abort(403, "شما به مدت 60 ثانیه مسدود شده‌اید.")
+
 @route('/')
 def my_route():
     return "<h1>TEST</h1>"
